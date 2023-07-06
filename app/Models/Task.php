5c53c9db7aa4d56b0722378470f8594bbdf27a08
Artem Rubets <artemrubets27@gmail.com>
2023-07-07 02:59:01 +0300

    Fixed seeding

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -28,8 +28,8 @@ class Task extends Model
     protected function deadline(): Attribute
     {
         return Attribute::make(
-            get: fn (string $value) => Carbon::createFromFormat('Y-m-d', $value)->format('d-m-Y'),
-            set: fn (string $value) => Carbon::createFromFormat('d-m-Y', $value)->format('Y-m-d')
+            get: fn (string|\DateTime $value) => Carbon::createFromFormat('Y-m-d', $value)->format('d-m-Y'),
+            set: fn (string|\DateTime $value) => Carbon::createFromFormat('d-m-Y', $value)->format('Y-m-d')
         );
     }
 
